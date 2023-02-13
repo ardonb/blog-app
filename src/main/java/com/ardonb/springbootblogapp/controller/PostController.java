86276c4c6558c83874375f95ecace9b1e6f39eb0
ardonb <ardon.bela@gmail.com>
2023-02-13 13:40:54 +0100

    add PostController and views for 404 and post

---
+++ src/main/java/com/ardonb/springbootblogapp/controller/PostController.java
@@ -0,0 +1,31 @@
+package com.ardonb.springbootblogapp.controller;
+
+import com.ardonb.springbootblogapp.model.Post;
+import com.ardonb.springbootblogapp.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+@Controller
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @GetMapping("/post/{id}")
+    public String getPost(@PathVariable Long id, Model model){
+        Optional<Post> optionalPost = postService.getById(id);
+
+        if(optionalPost.isPresent()){
+            Post post = optionalPost.get();
+            model.addAttribute("post", post);
+            return "post";
+        } else {
+            return "404";
+        }
+    }
+}
